4087e39409c7d3331aced4d3c8ade36b52061e89
leegakyeong <alaska7132@naver.com>
2020-07-13 22:36:11 +0900

    Updates

--- sketch.702e4367.js
+++ sketch.702e4367.js
@@ -2814,7 +2814,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54671" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49460" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
